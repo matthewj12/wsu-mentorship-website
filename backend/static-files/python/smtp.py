af54589ca052cedc6992d38d3df4b4fdd68cd675
Matthew Jilk <55640779+matthewj12@users.noreply.github.com>
2022-09-09 10:56:03 -0500

    updated email to new gmail account

--- backend/static-files/python/smtp.py
+++ backend/static-files/python/smtp.py
@@ -1,37 +1,28 @@
 import smtplib, sys
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
-sender_email_addr = 'eimyatnoeaung98@gmail.com'
+smtp_url = 'smtp.gmail.com'
+sender = 'wsuMentorshipVerify@gmail.com'
+app_specific_password = 'kwsgzcyxynvuetvr'
+recver = sys.argv[1]
 
-# The corresponding password for sender_email_addr
-# How to generate an application specific password: support.google.com/accounts/answer/185833?hl
-# This is Google's way of allowing "unsecure devices" to access your Gmail account.
-app_specific_password = 'nabubrqsybtvppic'
+msg = MIMEMultipart('alternative')
+msg['Subject'] = 'verify'
+text = 'Your verification code is: ' + sys.argv[2]
 
-
-recver_email_addr = sys.argv[1]
-verification_code = sys.argv[2]
-print(recver_email_addr)
-# recver_email_addr = 'eimyatnoeaung98@gmail.com'
-
-email_body_content = '\n\nEnter this verification code: ' + verification_code + '\n\nnext 5 minutes to finish confirming your email.'
 
 def main():
-	# Standard SMTP port
-	with smtplib.SMTP('smtp.gmail.com', 587) as smtp_server:
-		# Start the Transport Layer Security (which then requires us to sign in, although SMTP can be easily spoofed)
+	with smtplib.SMTP(smtp_url, 587) as smtp_server:
 		smtp_server.starttls()
+		smtp_server.login(sender, app_specific_password)
 
-		# Log in with your email address and aplication specific password
-		smtp_server.login(sender_email_addr, app_specific_password)
+		msg['From'] = 'wsuMentorshipVerify@gmail.com'
+		part1 = MIMEText(text, 'plain')
+		msg.attach(part1)
+		
+		smtp_server.sendmail(sender, recver, msg.as_string())
 
-		# Perform's an entire mail transaction
-		smtp_server.sendmail(sender_email_addr, recver_email_addr, email_body_content)
 
 if __name__ == '__main__':
-	try:
-		print('Destination address:', recver_email_addr)
-		main()
-		print('Email sent successfully.')
-	except Exception:
-		print('Exception occured while sending email.')
-
+	main()
